a8f16b746c10571c176427bf9deece5581f52a25
JYG <jojeayun900@gmail.com>
2022-08-26 23:52:51 +0900

    change css file

--- src/App.js
+++ src/App.js
@@ -37,54 +37,46 @@ function App() {
       </header>
       <aside>
         <div class="side_ui">
-          <div class="cartegory">
+          <S.cartegory>
             <S.Card><img class="home" src="home.png" /><S.CardText>홈</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>탐색</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>Shorts</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>구독</S.CardText></S.Card>
-          </div>
-          <div class="cartegory">
+          </S.cartegory>
+          <S.cartegory>
             <S.Card><img class="home" src="home.png" /><S.CardText>보관함</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>시청기록</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>내 동영상</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>나중에 볼 동영상</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>좋아요 표시한 동영상</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>재생목록</S.CardText></S.Card>
-          </div>
-          <div class="cartegory">
-            <p class="subscribe">구독</p>
+          </S.cartegory>
+          <S.cartegory>
+            <S.CG_font>구독</S.CG_font>
             <S.Card><img class="home" src="home.png" /><S.CardText>유튜버</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>유튜버</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>유튜버</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>유튜버</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>유튜버</S.CardText></S.Card>
-          </div>
-          <div class="cartegory">
-            <p class="subscribe">YOUTUBE 더보기</p>
+          </S.cartegory>
+          <S.cartegory>
+            <S.CG_font>YOUTUBE 더보기</S.CG_font>
             <S.Card><img class="home" src="home.png" /><S.CardText>YouTube Premium</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>영화</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>게임</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>실시간</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>학습</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>스포츠</S.CardText></S.Card>
-          </div>
-          <div class="cartegory">
+          </S.cartegory>
+          <S.cartegory>
             <S.Card><img class="home" src="home.png" /><S.CardText>설정</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>신고기록</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>고객센터</S.CardText></S.Card>
             <S.Card><img class="home" src="home.png" /><S.CardText>의견 보내기</S.CardText></S.Card>
-          </div>
-          <div class="cartegory">
-            <p> 대충 쓸모없는 글자들</p>
-            <p> 대충 쓸모없는 글자들</p>
-            <p> 대충 쓸모없는 글자들</p>
-            <p> 대충 쓸모없는 글자들</p>
-            <p> © 2022 Google LLC</p>
-            <p> CEO: 선다 피차이</p>
-            <p> 주소: 1600 Amphitheatre Parkway,</p>
-            <p> Mountain View, CA 94043, USA.</p>
-            <p> 전화: 080-822-1450(무료)</p>
-          </div>
+          </S.cartegory>
+          <S.cartegory>
+            <p>기타등등</p>
+          </S.cartegory>
         </div>
       </aside>
     </div>
