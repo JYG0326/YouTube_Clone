42036182acbc496f535ed95ced58eceb642f3820
JYG <jojeayun900@gmail.com>
2022-08-29 21:00:49 +0900

    add styled css

--- src/styled.ts
+++ src/styled.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+/*헤더 UI*/
 export const main_ui = styled.div`
     width:100%;
     height: 70px;
@@ -48,6 +49,7 @@ export const usermenu = styled.div`
 export const make = styled.img`
     margin: 0px 5px 20px 10px;
 `;
+
 export const menu = styled.img`
     width:30px;
     margin: 10px 10px 30px 10px;
@@ -56,14 +58,15 @@ export const menu = styled.img`
 export const alarm = styled.img`
     width:30px;
     margin: 10px 10px 30px 10px;
-  }
-`
+`;
+
 export const icon = styled.img`
     width:35px;
     border-radius: 100px;
-    margin:15px 30px 30px 10px
+    margin:15px 30px 30px 10px;
 `;
 
+/*상단 필터 사이드바*/
 export const sub_ui = styled.div`
     width: calc(100% - 240px);
     display: flex;
@@ -72,6 +75,7 @@ export const sub_ui = styled.div`
     float: right;
     border-bottom: 1px solid;
 `;
+
 export const filterbtn = styled.button`
     height:30px;
     border-radius: 30px;
@@ -80,25 +84,22 @@ export const filterbtn = styled.button`
     margin: 10px 5px 10px 5px;
     padding: 0 10px 0 10px;
 `;
-  
+
+/*좌측 사이드바*/
 export const side_ui = styled.div`
     width: 232.5px;
     height: calc(100vh - 72px);
     padding-right: 7.5px;
     box-sizing: border-box;
     overflow-y: scroll;
+    &:-webkit-scrollbar{
+        width: 10px;
+    };
+    &:-webkit-scrollbar-thumb{
+        background-color: #D3d3d3;
+        border-radius: 5px;
+    };
 `;
-
-/*
-export const side_ui::-webkit-scrollbar = styled.div`
-    width: 10px;
-`;
-
-export const side_ui::-webkit-scrollbar-thumb = styled.div`
-    background-color: #D3d3d3;
-    border-radius: 5px;
-`;
-*/
 
 export const cartegory = styled.div`
     padding: 10px 0px 10px 0px;
@@ -110,10 +111,16 @@ export const CG_font = styled.p`
 `;
 
 export const Card = styled.div`
-height: 40px;
+    height: 40px;
 `;
 
-export const CardText = styled.p`
+export const H_img = styled.img`
+    width:25px;
+    margin: 7.5px 20px 7.5px 20px;
+    float: left;
+`;
+
+export const CardText = styled.p` 
 font-size: 14px;
 height: 40px;
 line-height: 40px;
