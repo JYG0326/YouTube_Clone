b5d19635e4446f35166e0fc9d5c0a6fefc36c6b9
JYG <jojeayun900@gmail.com>
2022-08-31 20:54:54 +0900

    add toggle function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import './styled.ts'
 import * as S from './styled.ts'
+import React,{useState} from 'react';
 
 function App() {
+  const [isOpen, setMenu] = useState(false);  // 메뉴의 초기값을 false로 설정
+  
+  const toggleMenu = () => {
+        setMenu(isOpen => !isOpen); // on,off 개념 boolean
+    }
   return (
     <div className="App">
       <header className="App-header">
         <S.main_ui>
-          <S.list src="list.png" />
+          <S.list src="list.png" onClick={()=>toggleMenu()} />
           <S.logo src="YT_logo.png" />
           <S.searchbox>
             <S.searchinput type="CardText" placeholder="검색" />
@@ -34,6 +40,60 @@ function App() {
           <S.filterbtn>감상한 동영상</S.filterbtn>
           <S.filterbtn>새로운 맞춤 동영상</S.filterbtn>
         </S.sub_ui>
+        <aside className={isOpen ? "show-menu" : "hide-menu"}>
+            <S.side_long>
+                <S.cartegory>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>홈</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>탐색</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>Shorts</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>구독</S.CardText></S.Card>
+                </S.cartegory>
+                <S.cartegory>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>보관함</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>시청기록</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>내 동영상</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>나중에 볼 동영상</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>좋아요 표시한 동영상</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>재생목록</S.CardText></S.Card>
+                </S.cartegory>
+                <S.cartegory>
+                    <S.CG_font>구독</S.CG_font>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>유튜버</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>유튜버</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>유튜버</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>유튜버</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>유튜버</S.CardText></S.Card>
+                </S.cartegory>
+                <S.cartegory>
+                    <S.CG_font>YOUTUBE 더보기</S.CG_font>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>YouTube Premium</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>영화</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>게임</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>실시간</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>학습</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>스포츠</S.CardText></S.Card>
+                </S.cartegory>
+                <S.cartegory>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>설정</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>신고기록</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>고객센터</S.CardText></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /><S.CardText>의견 보내기</S.CardText></S.Card>
+                </S.cartegory>
+                <S.cartegory>
+                    <p>기타등등</p>
+                </S.cartegory>
+            </S.side_long>
+        </aside>
+        <aside className={isOpen ? "hide-menu" : "show-menu"}>
+            <S.side_short>
+                <S.cartegory>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /></S.Card>
+                    <S.Card><S.H_img src="home.png" alt="Icon_Home" /></S.Card>
+                </S.cartegory>
+            </S.side_short>
+        </aside>
       </header>
     </div>
   );
